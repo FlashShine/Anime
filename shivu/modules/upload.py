09d012ba2c9a32e102de7e2aa86f57b93e16d828
Philo69 <120897603+Philo69@users.noreply.github.com>
2024-07-27 11:47:35 +0530

    Update upload.py

--- shivu/modules/upload.py
+++ shivu/modules/upload.py
@@ -12,7 +12,7 @@ img_url character-name anime-name rarity-number
 
 use rarity number accordingly rarity Map
 
-rarity_map = 1 (⚡️ Elite), 2 (🥂 Legendary) , 3 ( 🕊️ Common), 4 (🔮 Mythical)"""
+rarity_map = 1 (🕊️ Common), 2 (🌟 Epic ) , 3 (🍷 Legendary), 4 (🔮 Mythical)"""
 
 
 
@@ -48,7 +48,7 @@ async def upload(update: Update, context: CallbackContext) -> None:
             await update.message.reply_text('Invalid URL.')
             return
 
-        rarity_map = {1: "⚡️ Elite", 2: "🥂 Legendary", 3: "🕊️ Common", 4: "🔮 Mythical"}
+        rarity_map = {1: "🕊️ Common", 2: "🌟 Epic", 3: "🍷 Legendary", 4: "🔮 Mythical"}
         try:
             rarity = rarity_map[int(args[3])]
         except KeyError:
