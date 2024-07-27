f4f8c0d04b8d1a354089cd37585fb4b07f5bac7a
Philo69 <120897603+Philo69@users.noreply.github.com>
2024-07-27 11:05:43 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -3,14 +3,14 @@ class Config(object):
 
     # Get this value from my.telegram.org/apps
     OWNER_ID = "7202072688"
-    sudo_users = "7222795580", "6180999156", "7202072688"
+    sudo_users = "7222795580", "6180999156", "7202072688" , "1284797486"
     GROUP_ID = -1001548130580
-    TOKEN = "6923226184:AAEhP0vCfJ8_i6hnzC4WuLMoOdaz0KUzDpw"
+    TOKEN = "6811413507:AAGIJ-8b9FLADJtTEvcBdjLBsCQh9FDKItc"
     mongo_url = "mongodb+srv://PhiloWise:Philo@waifu.yl9tohm.mongodb.net/?retryWrites=true&w=majority&appName=Waifu"
     PHOTO_URL = ["https://telegra.ph/file/8d9c4af9b7705b1aafc99.jpg", "https://telegra.ph/file/8d9c4af9b7705b1aafc99.jpg"]
     SUPPORT_CHAT = "PhiloMusicSupport"
     UPDATE_CHAT = "TechPiroBots"
-    BOT_USERNAME = "@WaifusGacha_bot"
+    BOT_USERNAME = "@Husbando_Snatch_Bot"
     CHARA_CHANNEL_ID = "-1002225093492"
     api_id = 26626068
     api_hash = "bf423698bcbe33cfd58b11c78c42caa2"
