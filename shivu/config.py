0a098509712cbc1e6b6e6fecf3fae23202244713
Oᴠᴇʀ ❖ Sᴛʀɪᴘᴇ <120897603+OverStripe@users.noreply.github.com>
2024-12-17 09:49:09 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -5,10 +5,10 @@ class Config(object):
     OWNER_ID = "7222795580"
     sudo_users = "7222795580", "6180999156" , "7032905213" , "7177206350"
     GROUP_ID = -1001548130580
-    TOKEN = "7579121046:AAEZ390bKvfuK04iF8lAVhO0lO14gYtaqzA"
+    TOKEN = "7579121046:AAErJ-zh2HE8zaJTY5V6B0Nkd1nztbhjb3Y"
     mongo_url = "mongodb+srv://PhiloWise:Philo@waifu.yl9tohm.mongodb.net/?retryWrites=true&w=majority&appName=Waifu"
     PHOTO_URL = ["https://telegra.ph/file/8d9c4af9b7705b1aafc99.jpg", "https://telegra.ph/file/8d9c4af9b7705b1aafc99.jpg"]
-    SUPPORT_CHAT = "PhiloMusicSupport"
+    SUPPORT_CHAT = "TechPiroBots"
     UPDATE_CHAT = "TechPiroBots"
     BOT_USERNAME = "@Husbando_Snatch_Bot"
     CHARA_CHANNEL_ID = "-1002225093492"
